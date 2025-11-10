3649172527d6fb0840f5a84203ee5883aaad7cb9
Evandro Sacramento II <evandro-dev@outlook.com>
2025-11-10 15:59:26 -0300

    desabilitando contexto de teste

--- src/test/java/com/essjr/Cadastro/CadastroApplicationTests.java
+++ src/test/java/com/essjr/Cadastro/CadastroApplicationTests.java
@@ -1,9 +1,11 @@
 package com.essjr.Cadastro;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("Ignorado no pipeline, pois carrega o contexto completo")
 class CadastroApplicationTests {
 
 	@Test
